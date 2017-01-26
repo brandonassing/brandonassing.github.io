78cb0c9d0e55c04e8cc8014858e949e8c11f99d8
brandonassing <bassing@uwo.ca>
2017-01-26 13:38:54 -0500

    fixed navbar overlap

--- scripts/photography.js
+++ scripts/photography.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 
     for (var i = 29; i >= 1; i--) {
         //figure out how to only load some, or use a proxy
-        $("#image-list").append('<li><div class = "photo-div col l3"><img class = "materialboxed photo" src="./img/Photos/' + String(i) + '.JPG"></div></li>');
+        $("#image-list").append('<li><div class = "photo-div col l3 m4 s6"><img class = "materialboxed photo" src="./img/Photos/' + String(i) + '.JPG"></div></li>');
     }
     $('.materialboxed').materialbox();
 });
