41d7803619f4205652d3d78630476006313d9622
brandonassing <bassing@uwo.ca>
2017-01-26 12:06:40 -0500

    commented out image display for push

--- scripts/photography.js
+++ scripts/photography.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
     var imageSRCs;
 
     for (var i = 29; i >= 1; i--) {
-        $("#image-list").append('<li><div class = "photo-div col l3"><img class = "photo" src="./img/Photos/' + String(i) + '.JPG"></div></li>');
+        //figure out how to only load some, or use a proxy
+        //$("#image-list").append('<li><div class = "photo-div col l3"><img class = "photo" src="./img/Photos/' + String(i) + '.JPG"></div></li>');
     }
 });
