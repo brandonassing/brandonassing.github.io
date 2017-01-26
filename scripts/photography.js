6e27df8cba48b75554cfa63c270b966f65bc7a46
brandonassing <bassing@uwo.ca>
2017-01-26 12:56:36 -0500

    added materialbox

--- scripts/photography.js
+++ scripts/photography.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
 
     for (var i = 29; i >= 1; i--) {
         //figure out how to only load some, or use a proxy
-        //$("#image-list").append('<li><div class = "photo-div col l3"><img class = "photo" src="./img/Photos/' + String(i) + '.JPG"></div></li>');
+        $("#image-list").append('<li><div class = "photo-div col l3"><img class = "materialboxed photo" src="./img/Photos/' + String(i) + '.JPG"></div></li>');
     }
+    $('.materialboxed').materialbox();
 });
