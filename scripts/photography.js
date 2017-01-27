8daddbb7be774f9f81daef83491fa793aaf4b2cb
brandonassing <bassing@uwo.ca>
2017-01-27 14:12:35 -0500

    semi photo fix

--- scripts/photography.js
+++ scripts/photography.js
@@ -15,7 +15,23 @@ $(document).ready(function() {
         //figure out how to only load some, or use a proxy
         imageSRCs[i] = "./img/Photos/" + String(i) + ".JPG";
 
-        $("#image-list").append('<li><div class = "photo-div col l3 m4 s6"><img class = "materialboxed photo" src="' + imageSRCs[i] + '"></div></li>');
+        /////////////////////////////////////////
+        //////KINDA WORKS, STILL LAGS/////////////
+        $("#image-list").append('<li><div class = "photo-div col l3 m4 s6"><img class = "materialboxed photo hidden" src="' + imageSRCs[i] + '"></div></li>');
+        $("img[src='" + imageSRCs[i] + "']").on("load", function() {
+            //window.alert($(this).attr("src"));
+            $(this).removeClass("hidden");
+        });
+        /////////////////////////////////////////
+        /*
+        var imgSRC = new Image();
+        imgSRC.src = imageSRCs[i];
+        $(imgSRC).on("load", function() {
+            //window.alert($(this).attr("src"));
+            $(this).removeClass("hidden");
+        });
+        */
+        /////////////////////////////////////////
         /*
         $('<img src="' + imageSRCs[i] + '">').on("load", function(i) {
             //$(this).appendTo('#some_target');
@@ -26,5 +42,21 @@ $(document).ready(function() {
         });
         */
     }
+    $(".photo-div").show();
+    var nImages = $("#all-images").length;
+    var loadCounter = 0;
+    //binds onload event listner to images
+    $("#all-images img").on("load", function() {
+        loadCounter++;
+        if (nImages == loadCounter) {
+            $(this).parent().show();
+            $("#loadingDiv").hide();
+        }
+    }).each(function() {
+
+        // attempt to defeat cases where load event does not fire
+        // on cached images
+        if (this.complete) $(this).trigger("load");
+    });
     $('.materialboxed').materialbox();
 });
