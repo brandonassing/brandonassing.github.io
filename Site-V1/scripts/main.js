e8b7a84e7bc24dcde708fb73ed2fb8b810a9cd03
brandonassing <bassing@uwo.ca>
2017-02-14 18:00:09 -0500

    reorganized dir for new site

---
+++ Site-V1/scripts/main.js
@@ -0,0 +1,95 @@
+$(document).ready(function() {
+    $(function() {
+        $('a[href*="#"]:not([href="#"])').click(function() {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
+    });
+
+    //=========================Header mobile resize====================
+    if ($(window).width() <= 600) {
+        var screenHeight = $(window).height();
+        $('#header').css('height', screenHeight + 'px');
+    } else if ($(window).width() <= 1024) {
+        var screenHeight = $(window).height() * 0.75;
+        $('#header').css('height', screenHeight + 'px');
+    } else {
+        var screenHeight = $(window).height() * 0.78;
+        $('#header').css('height', screenHeight + 'px');
+    }
+    //=========================Header mobile resize====================
+
+    $(window).scroll(function() {
+        $(".scroll-fade-out").css("opacity", 1 - $(window).scrollTop() / 350);
+    });
+    $(window).scroll(function() {
+        $(".scroll-fade-in").css("opacity", $(window).scrollTop() / 700);
+    });
+
+    //=========================Image Load====================
+    //TODO add image array; fade between; src from http://css3.bradshawenterprises.com/cfimg/
+    var bgimg = new Image();
+    bgimg.src = './img/PixelPush.png';
+    $(bgimg).on("load", function() {
+        $("#header").delay(300).addClass("bg", function() {
+            //TODO change to background animate, not entire header
+            $('#header').animate({
+                opacity: 1
+            }, {
+                duration: 3000
+            });
+            $("#headshot")
+                .delay(2000)
+                .queue(function() {
+                    $(this).removeClass("hidden");
+                    $(this).addClass("fadeInDown");
+                    $(this).dequeue();
+                });
+            $('#typingSubheading').typeIt({
+                speed: 120,
+                breakLines: false,
+                autoStart: false
+            }).tiPause(1700).tiDelete(11).tiPause(300).tiType("Software Engineer").tiPause(500).tiSettings({
+                speed: 150
+                    //TODO add break for mobile
+            }).tiType(" : BESc - Class of 2018");
+        });
+
+    });
+
+    //=========================Image Load====================
+
+    /*
+        $(document).scroll(function () {
+            scroll_pos = $(this).scrollTop();
+            if (scroll_pos > 210) {
+                //$("#about").css('background-color', '#4db6ac');
+                $("#about").stop().animate({
+
+                    //for about background tranparency on page top
+                    //backgroundColor: 'rgba(128,203,196,0)'
+
+                    backgroundColor: '#4db6ac'
+                }, 300);
+            } else {
+                //$("#about").css('background-color', '#80cbc4');
+                $("#about").stop().animate({
+
+                    //for about background tranparency on page top
+                    //backgroundColor: 'rgba(128,203,196,1)'
+
+                    backgroundColor: '#0e4d44'
+                        //TODO change this to animate background image somehow
+                }, 300);
+            }
+        });
+    */
+});
