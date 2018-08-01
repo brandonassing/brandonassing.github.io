08157c01c72a24d6a89433a37f986df4975085a9
brandon.assing <brandon.assing@ibm.com>
2018-08-01 10:01:10 -0400

    added some project cards and scroll function

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,9 @@
-//TODO on tab click scroll to top of tab bar
 //TODO change tab: remove bar and add overlay
 
 $(document).ready(function() {
-  
+  $('.tab-text').click(function(){
+    $('html, body').animate({
+            scrollTop: $("#tabs-container").offset().top
+        }, 700);
+  });
 });
