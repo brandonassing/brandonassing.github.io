275b902fe158313d4202e7a622e313951e6ff80b
Brandon Assing <assing.brandon@gmail.com>
2018-09-09 21:05:31 -0400

    idk

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,6 @@
 //TODO change tab: remove bar and add overlay
 //TODO add load buffer
+//TODO fix scroll on menu tab click; doesn't scroll to same spot bc SKILLS isn't long enough
 $(document).ready(function() {
   $('.tab-text').click(function(){
     $('html, body').animate({
